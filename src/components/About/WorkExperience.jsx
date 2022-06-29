6f1deaa20dacc759e3b239ed6da77427a28fe3d2
Erwin Medina <erwin.l.medina@gmail.com>
2022-06-29 15:35:50 -0700

    hopefully this fixes my problems

--- src/components/About/WorkExperience.jsx
+++ src/components/About/WorkExperience.jsx
@@ -19,7 +19,6 @@ export default function WorkExperience() {
         setRevel(!revel);
         setKnox(!knox);
     }
-    // EXTRA LINE
 
     return (
         <div className="education container">
