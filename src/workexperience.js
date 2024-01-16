2bdda8a5ba83f9b8eaac2fa881888607a28887a6
Erwin Medina <erwin.l.medina@gmail.com>
2024-01-16 11:30:15 -0800

    Fixed images on project page. Added 1 more work exp

--- src/workexperience.js
+++ src/workexperience.js
@@ -22,7 +22,7 @@ export const workexperience = [
         logo:       "https://i.imgur.com/GGHOhcp.jpg",
         name:       "VCI Construction, LLC",
         shortname:  "VCI",
-        time:       "2021-2023",
+        time:       "2021-2024",
         position:   "Project Data Analyst",
         description: [
             "Database Management",
@@ -53,6 +53,16 @@ export const workexperience = [
             "Sprints",
             "Stand Ups",
             "Visual Studio",
+        ]
+    },
+    {
+        logo:       "https://i.imgur.com/hqiI1Qy.png",
+        name:       "Paramount+",
+        shortname:  "Paramount",
+        time:       "Summer 2024",
+        position:   "Software Engineering Intern",
+        description: [
+            "TBD"
         ]
     },
 ]
