03f42f96743d128c3fa97928a6eca9dab2c31eb6
Erwin <erwin.l.medina@gmail.com>
2024-09-16 10:44:54 -0700

    removed footer for now

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -16,7 +16,7 @@ export default function HomePage() {
             <HomeExperience/>
             <HomeSkills/>
             <HomeProjects/>
-            <HomeFooter/>
+            {/* <HomeFooter/> */}
         </div>
     )
 }
